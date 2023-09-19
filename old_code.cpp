a2ea8921372e2376128791f2cd98cda442216298
Ruan Molgero <ruanmolgero@gmail.com>
2023-09-19 20:09:36 -0300

    Add code for s1.3, repo needs refactor

--- old_code.cpp
+++ old_code.cpp
@@ -1,6 +1,8 @@
+#include <vector>
 #include <iostream>
 #include <unordered_map>
 
+// old_challenge_s1.1
 // Tradução de entradas hex para binário usando hash
 std::string hexToBinary(const std::string &hex)
 {
@@ -85,8 +87,150 @@ std::string binToBase64(const std::string &bin)
     return base64;
 }
 
+// old_challenge_s1.3
+
+std::vector<uint8_t> repeatableXOR(const std::vector<uint8_t> &byteArray, uint8_t singleChar)
+{
+    // Initialize the result array
+    std::vector<uint8_t> result(byteArray.size());
+
+    // Perform the XOR operation
+    for (size_t i = 0; i < byteArray.size(); ++i)
+    {
+        result[i] = singleChar ^ byteArray[i];
+    }
+
+    return result;
+}
+
+std::unordered_map<char, double> expected_frequency_texts = {
+    {'e', 12.7},
+    {'t', 9.1},
+    {'a', 8.2},
+    {'o', 7.5},
+    {'i', 7.0},
+    {'n', 6.7},
+    {'s', 6.3},
+    {'h', 6.1},
+    {'r', 6.0},
+    {'d', 4.3},
+    {'l', 4.0},
+    {'c', 2.8},
+    {'u', 2.8},
+    {'m', 2.4},
+    {'w', 2.4},
+    {'f', 2.2},
+    {'g', 2.0},
+    {'y', 2.0},
+    {'p', 1.9},
+    {'b', 1.5},
+    {'v', 0.98},
+    {'k', 0.77},
+    {'j', 0.15},
+    {'x', 0.15},
+    {'q', 0.095},
+    {'z', 0.074},
+};
+
+// std::unordered_map<char, double> expected_frequency_dictionaries = {
+//     {'e', 11.16}, {'a', 8.50},
+//     {'r', 7.58},  {'i', 7.54},
+//     {'o', 7.16},  {'t', 6.95},
+//     {'n', 6.65},  {'s', 5.74},
+//     {'l', 5.49},  {'c', 4.54},
+//     {'u', 3.63},  {'d', 3.38},
+//     {'p', 3.17},  {'m', 3.01},
+//     {'h', 3.00},  {'g', 2.47},
+//     {'b', 2.07},  {'f', 1.82},
+//     {'y', 1.78},  {'w', 1.29},
+//     {'k', 1.10},  {'v', 1.01},
+//     {'x', 0.29},  {'z', 0.27},
+//     {'j', 0.2},   {'q', 0.2},
+// };
+
+std::unordered_map<char, int> calculateFrequency(const std::string &text)
+{
+    std::unordered_map<char, int> frequency;
+
+    for (char c : text)
+    {
+        if (isalpha(c))
+        {
+            frequency[tolower(c)]++;
+        }
+    }
+
+    return frequency;
+}
+
+std::string byteArrayToString(const std::vector<unsigned char> &byteArray)
+{
+    return std::string(byteArray.begin(), byteArray.end());
+}
+
+double textScore(const std::string &text)
+{
+    double score = 0;
+
+    std::unordered_map<char, int> text_frequency = calculateFrequency(text);
+
+    for (const auto &pair : text_frequency)
+    {
+        char letter = pair.first;
+        int count = pair.second;
+        if (expected_frequency_texts.count(letter) > 0)
+        {
+            double expected_percent = expected_frequency_texts[letter];
+            double actual_percent = (static_cast<double>(count) / text.length()) * 100.0;
+            // Usando a diferença absoluta tem-se um score que representa uma correlação inversa entre
+            // as frequências do texto dado e esperadas
+            score += std::abs(expected_percent - actual_percent);
+        }
+    }
+
+    return score;
+}
+
+// std::string string_to_hex(const std::string &input)
+// {
+//     static const char hex_digits[] = "0123456789abcdef";
+
+//     std::string output;
+
+//     output.reserve(input.length() * 2);
+//     for (unsigned char c : input)
+//     {
+//         output.push_back(hex_digits[c >> 4]);
+//         output.push_back(hex_digits[c & 15]);
+//     }
+
+//     return output;
+// }
+
+std::string char_to_hex(char input)
+{
+    static const char hex_digits[] = "0123456789abcdef";
+
+    std::string output;
+    output.push_back(hex_digits[static_cast<unsigned char>(input) >> 4]);
+    output.push_back(hex_digits[static_cast<unsigned char>(input) & 15]);
+
+    return output;
+}
+
 int main()
 {
     std::cout << "Hello, C++ on Linux!" << std::endl;
+
+    // old_challenge_s1.3
+    //  const std::string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    //  std::string hex_string = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
+    //  std::cout << std::hex << string_to_hex("x");
+    //  for (char c : chars)
+    //  {
+    //      std::cout << "Character: " << c << " Score: " << textScore(byteArrayToString(repeatableXOR(hexToByteArray(hex_string), c))) << std::endl;
+    //  }
+    //  std::cout << byteArrayToString(repeatableXOR(hexToByteArray(hex_string), 'w')) << std::endl;
+
     return 0;
 }
